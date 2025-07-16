57486ab56fb3ba1b6680132fad5ad6415204d642
Bianca Fonseca <b.bianca.fonseca@gmail.com>
2025-07-16 15:18:01 -0300

    string

---
+++ .history/string_20250716142609.py
@@ -0,0 +1,6 @@
+"""
+Etapa 1: métodos úteis string
+Etapa 2: interpolação de strings
+Etapa 3: fatiamento de strings
+Etapa 4: string múltiplas linhas
+"""
---
+++ .history/string_20250716151547.py
@@ -0,0 +1,42 @@
+"""
+Etapa 1: métodos úteis string
+Etapa 2: interpolação de strings
+Etapa 3: fatiamento de strings
+Etapa 4: string múltiplas linhas ou triplas aspas dentro do print() pra ficar
+melhor a visualização
+"""
+
+curso = "Python"
+print(curso.center(10, "#")) # width=10, fillchar='#'
+
+print(".".join(curso))
+# .center(): A string a ser centralizada. Retorna ela centralizada e preenchida
+# .join(): A string separadora. Junta os itens do iterável usando ela
+
+# Ou seja:
+# texto.center() → "centraliza este texto".
+# "separador".join() → "use este separador para juntar os itens".
+
+
+greeting = "   Olá, mundo!   "
+print(greeting + ".")
+print(greeting.strip() + ".")
+print(greeting.lstrip()+ ".")
+print(greeting.rstrip() + ".")
+
+#    Olá, mundo!   .
+# Olá, mundo!.
+# Olá, mundo!   .
+#    Olá, mundo!.
+
+
+# Aspas triplas dentro do print()
+menu = "[1] Iniciar [2] Sair "
+print(
+    """
+    ========= MENU =========
+    [1] Iniciar 
+    [2] Sair
+    ========================
+    """
+)
